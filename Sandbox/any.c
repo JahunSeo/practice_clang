9ff80ccffb4ad35e267da3ba09ecb69bbab23436
JahunSeo <wkgjs0809@gmail.com>
2021-09-08 19:28:46 +0900

    [johnny] check

--- Sandbox/any.c
+++ Sandbox/any.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 int main(void) {
     int n;
-    scanf("%d", &n);
-    
-    for (int i=1; i<=n; i++) {
-        printf("%d\n",i);
+    int a, b;
+    int ans = 0;
+    scanf("%d\n", &n);
+    printf("n: %d\n", n);
+    for (int i=0; i < n; i++) {
+        scanf("%d %d\n", &a, &b);
+        ans = 1;
+        for (int k=0; k < b; k++) {
+            ans = (ans * a)%10;
+            printf(" %d ", ans);
+        }
+        printf("%d\n", ans);
     }
-  
     return 0;
 }
