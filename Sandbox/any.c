7fe2039f9be78cd853702ff180ad39b489387c9c
JahunSeo <wkgjs0809@gmail.com>
2021-09-10 17:39:52 +0900

    [johnny] check

--- Sandbox/any.c
+++ Sandbox/any.c
@@ -2,17 +2,17 @@
 int main(void) {
     int n;
     int a, b;
-    int ans = 0;
-    scanf("%d\n", &n);
-    printf("n: %d\n", n);
-    for (int i=0; i < n; i++) {
-        scanf("%d %d\n", &a, &b);
-        ans = 1;
-        for (int k=0; k < b; k++) {
-            ans = (ans * a)%10;
-            printf(" %d ", ans);
-        }
-        printf("%d\n", ans);
-    }
+    int ans = 3;
+    printf("%lu\n", sizeof(char));
+    printf("%lu\n", sizeof(int));
+    printf("%lu\n", sizeof(long));
+    printf("%lu\n", sizeof(float));
+    printf("\n");
+    printf("%x %x %x\n", 32, 32 | 0, 32 | 1);
+    printf("%x %x %x\n", ~7, 8 & ~7, 9 & ~7);
+
+    printf("*(unsigned int *)(%p) = (%d)\n", &a, ans);
+
+
     return 0;
 }
